7184f97f8f3a246cd8450c7eed98293d46b621f7
Jussi Hietanen <jussi.hietanen@aalto.fi>
2019-05-22 19:43:38 +0300

    Implement bitmap operations

--- include/uranos/arch.h
+++ include/uranos/arch.h
@@ -15,6 +15,9 @@ typedef uint32_t bm_element;
 
 #define MAX_PAGE_FRAMES 32768 // 4 GiB
 
+#define BITS_PER_LONG 32
+#define LONG_MAX_VALUE 4294967295
+
 #endif
 
 void init_arch(void);
--- lib/bitmap.c
+++ lib/bitmap.c
@@ -14,31 +14,56 @@ size_t bm_sizeof(bitmap_t* bm)
 
 void bm_set_bit_value(bitmap_t* bm, size_t idx, uint8_t value)
 {
+  bm[idx/BITS_PER_LONG] |= (!!value << (BITS_PER_LONG-idx-1));
 	return;
 }
 
 void bm_set_bit_off(bitmap_t* bm, size_t idx)
 {
+  bm[idx/BITS_PER_LONG] &= ~(1 << (BITS_PER_LONG-idx-1));
 	return;
 }
 
 void bm_set_bit_on(bitmap_t* bm, size_t idx)
 {
+  bm[idx/BITS_PER_LONG] &= (1 << (BITS_PER_LONG-idx-1));
 	return;
 }
 
 void bm_set_bit_flip(bitmap_t* bm, size_t idx)
 {
+  bm[idx/BITS_PER_LONG] ^= (1 << (BITS_PER_LONG-idx-1));
 	return;
 }
 
 uint8_t bm_test_bit(bitmap_t* bm, size_t idx)
 {
-	return 0;
+  return ((bm[idx/BITS_PER_LONG] >> (BITS_PER_LONG-idx-1)) & 1);
 }
 
 void bm_set_all(bitmap_t* bm, uint8_t value)
 {
+  int chunks = bm_sizeof(bm) / BITS_PER_LONG;
+  for(int i = 0; i < chunks; i++)
+  {
+    if(value)
+    {
+      bm->bits[i] = LONG_MAX_VALUE;
+    }
+    else
+    {
+      bm->bits[i] = 0;
+    }
+  }
+
+  int remainder = bm_sizeof(bm) % BITS_PER_LONG;
+  if(remainder)
+  {
+    for(int i = 0; i < remainder; i++)
+    {
+      bm_set_bit_value(bm, chunks * BITS_PER_LONG + i, value);
+    }
+  }
 
 	return;
 }
